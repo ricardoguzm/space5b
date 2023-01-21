7f76c4265b4f9ec6f9fba7f770d77e48c59be83a
snipgamer1 <chuchis619@hotmail.com>
2023-01-20 21:21:45 -0600

    two combio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface Rocket{
+  name: String;
+  description: String;
+  flickr_images: String;
+  engine: String;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spacex.5b';
+  title = 'space-5B';
+  data:  Rocket[] = [];
+  capsule : any [] = [];
+  company : any [] = [];
+
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit() {
+    this.http.get<Rocket[]>('https://api.spacexdata.com/v4/rockets').subscribe((data: any)  => {
+      this.data = data;
+    })
+    this.http.get('https://api.spacexdata.com/v4/capsules').subscribe((capsule: any)  => {
+      this.capsule = capsule;
+    })
+    this.http.get('https://api.spacexdata.com/v4/company').subscribe((company: any)  => {
+      this.company = company;
+    })
+    
+  }
 }
